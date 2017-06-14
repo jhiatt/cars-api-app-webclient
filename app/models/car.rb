d42e3a63b3f449a2edbe67643aba42849793e8e7
Jordan Hiatt <jordan.hiatt11@gmail.com>
2017-06-13 21:10:15 -0400

    added .env

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,8 +2,8 @@ class Car
   attr_accessor :vin, :manufacture, :id
   URL = "http://localhost:3001/api/v2"
   HEADER =  {"Accept" => "application/json",
-                      "Authorization" => "Token token=coolbeans",
-                      "X-User-Email" => "jordan@hiatt.com"
+                      "Authorization" => "Token token=#{ENV['API_KEY']}",
+                      "X-User-Email" => ENV['EMAIL']
                     }
 
   def initialize(hash)
@@ -14,21 +14,21 @@ class Car
 
   def self.all
     array_cars = []
-    Unirest.get("#{URL}/cars", headers: HEADER).body.each do |hash|
+    Unirest.get("#{ENV['URL']}/cars", headers: HEADER).body.each do |hash|
       array_cars <<  Car.new(hash)
     end
     array_cars
   end
 
   def self.find_by(input)
-    hash = Unirest.get("#{URL}/cars/#{input[:id]}",
+    hash = Unirest.get("#{ENV['URL']}/cars/#{input[:id]}",
                           headers: HEADER, 
                         ).body
     car = Car.new(hash)
   end
 
   def self.create(hash)
-        car = Unirest.post("#{URL}/cars.json", 
+        car = Unirest.post("#{ENV['URL']}/cars.json", 
                           headers: HEADER, 
                           parameters: {vin: hash[:vin],
                                         manufacture: hash[:manufacture]}
@@ -36,13 +36,13 @@ class Car
   end
 
   def destroy
-        car = Unirest.delete("#{URL}/cars/#{id}",
+        car = Unirest.delete("#{ENV['URL']}/cars/#{id}",
                             headers: HEADER, 
                               ).body
   end
 
   def update(hash)
-        hash_1 = Unirest.patch("#{URL}/cars/#{self.id}.json", 
+        hash_1 = Unirest.patch("#{ENV['URL']}/cars/#{self.id}.json", 
                           headers: HEADER, 
                           parameters: {vin: hash[:vin],
                                         manufacture: hash[:manufacture]}
